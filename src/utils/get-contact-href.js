243edc76f8495266f53f99ddf37bf56071c820b0
Your Name <you@example.com>
2019-04-13 14:11:07 +0700

    update social icon

--- src/utils/get-contact-href.js
+++ src/utils/get-contact-href.js
@@ -8,11 +8,8 @@ const getContactHref = (name, contact) => {
     case 'github':
       href = `https://github.com/${contact}`;
       break;
-    case 'vkontakte':
-      href = `https://vk.com/${contact}`;
-      break;
-    case 'telegram':
-      href = `telegram:${contact}`;
+    case 'medium':
+      href = `https://medium.com/@:${contact}`;
       break;
     case 'email':
       href = `mailto:${contact}`;
